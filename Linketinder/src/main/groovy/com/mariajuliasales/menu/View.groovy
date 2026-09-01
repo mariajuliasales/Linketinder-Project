9ec62f2d3ce3ae281b5c243556c262accf8af597
mariajuliasales <salesmariajulia11@gmail.com>
2026-09-01 13:35:06 -0300

    feat: add View class with menu display

---
+++ Linketinder/src/main/groovy/com/mariajuliasales/menu/View.groovy
@@ -0,0 +1,17 @@
+package com.mariajuliasales.menu
+
+class View {
+
+    View(){}
+
+    static void showMenu() {
+        println "Bem vindo(a) ao LinkeTinder!"
+        println "Escolha uma opção:"
+        println "1. Listar todos os candidatos"
+        println "2. Listar todas as empresas"
+        println "3. Criar um novo candidato"
+        println "4. Criar uma nova empresa"
+        println "5. Sair"
+    }
+
+}
