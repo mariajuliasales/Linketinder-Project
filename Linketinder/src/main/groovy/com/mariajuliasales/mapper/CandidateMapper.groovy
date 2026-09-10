b093f106b3d5c74f809e6bbd86bab81563085e26
mariajuliasales <salesmariajulia11@gmail.com>
2026-09-10 15:41:50 -0300

    feat: add CandidateResponse and mapping method in CandidateMapper

--- Linketinder/src/main/groovy/com/mariajuliasales/mapper/CandidateMapper.groovy
+++ Linketinder/src/main/groovy/com/mariajuliasales/mapper/CandidateMapper.groovy
@@ -1,6 +1,7 @@
 package com.mariajuliasales.mapper
 
 import com.mariajuliasales.dto.request.CandidateRequest
+import com.mariajuliasales.dto.response.CandidateResponse
 import com.mariajuliasales.model.Candidate
 import com.mariajuliasales.model.Competence
 
@@ -21,6 +22,23 @@ class CandidateMapper {
                 request.competences()?.collect { it as Competence } ?: [],
                 request.cpf(),
                 request.age()
+        )
+    }
+
+    static CandidateResponse toCandidateResponse(Candidate candidate) {
+
+        if(!candidate)
+            throw new IllegalArgumentException("Candidate cannot be null")
+
+        new CandidateResponse(
+                candidate.name,
+                candidate.email,
+                candidate.state,
+                candidate.cep,
+                candidate.description,
+                candidate.competences?.collect { it as Competence } ?: [],
+                candidate.cpf,
+                candidate.age
         )
     }
 }
